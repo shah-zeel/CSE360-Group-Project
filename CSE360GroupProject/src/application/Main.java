f97c45f0ae846a1ab36bf0789ec051599d12b944
shah-zeel <zeelshah2708@gmail.com>
2024-10-06 23:30:45 -0700

    Basic Setup

---
+++ CSE360GroupProject/src/application/Main.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		System.out.println("To");
+		System.out.println("GeeksforGeeks");
+	}
+}
